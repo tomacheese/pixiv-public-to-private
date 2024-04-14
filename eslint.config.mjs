58f97c38b48a1edcaa76b5d9feeeeb5af1e68e72
Tomachi[MINT] <tomachi@tomacheese.com>
2024-04-14 15:07:34 +0900

    feat: support eslint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+import eslint from '@eslint/js'
+import tseslintParser from '@typescript-eslint/parser'
+import eslintPrettier from 'eslint-config-prettier'
+import eslintNode from 'eslint-plugin-n'
+import unicorn from 'eslint-plugin-unicorn'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintNode.configs['flat/recommended-script'],
+  // @ts-expect-error flat/recommendedの返すpluginsがstring[]なことでエラーになるため
+  unicorn.configs['flat/recommended'],
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      globals: {
+        ...globals.es2020,
+        ...globals.node,
+      },
+      parser: tseslintParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      'unicorn/prefer-top-level-await': 'off',
+      'unicorn/no-null': 'off',
+      'no-console': 'off',
+    },
+    ignores: ['dist', 'output', 'node_modules', 'data', 'logs'],
+  },
+  eslintPrettier
+)
